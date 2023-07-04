10c85e8ede23990706ed2725a48f3bf111b30135
jimaa maya <jimaa.maya@gmail.com>
2023-07-04 18:45:30 +0300

    2nd

--- src/app.js
+++ src/app.js
@@ -1,16 +1,9 @@
 const express = require("express");
-const app = express();
-
-
-
 const db = require("./db/connection");
 
-
-
-
-
-
+const app = express();
 const port = 3000;
+
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
-})
+});
