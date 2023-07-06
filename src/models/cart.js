e2ab5a8f2765ebe8fd08499d08cc037263d5fe39
jimaa maya <jimaa.maya@gmail.com>
2023-07-06 19:27:07 +0300

    Adding_timeStamp

--- src/models/cart.js
+++ src/models/cart.js
@@ -1,34 +1,35 @@
 const mongoose = require('mongoose');
 
-const cartSchema = new mongoose.Schema({
-    cart_id: {
-        type: String,
-        required: true,
-        unique: true
+const cartSchema = new mongoose.Schema(
+    {
+        cart_id: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User',
+        },
+        dish_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Dishes',
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: ['active', 'inactive', 'completed'],
+            default: 'active',
+        },
     },
-    user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-    },
-    dish_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Dishes'
-    },
-    quantity: {
-        type: Number,
-        required: true
-    },
-    addedDate: {
-        type: Date,
-        default: Date.now
-    },
-    status: {
-        type: String,
-        enum: ['active', 'inactive', 'completed'],
-        default: 'active'
+    {
+        timestamps: true,
     }
-});
+);
 
-module.exports = mongoose.model("cart", cartSchema);
+module.exports = mongoose.model('cart', cartSchema);
