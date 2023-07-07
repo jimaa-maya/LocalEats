c16d219faa9fbb7ed0b6368a6ba5d1a16ea8f2d5
Mohamad Aid Bacuk Zanab <mohammedsharif067@gmail.com>
2023-07-07 12:50:55 +0300

    fixing_namings& timestamps

--- src/models/orders.js
+++ src/models/orders.js
@@ -2,35 +2,41 @@ const mongoose = require('mongoose');
 
 const ordersSchema = new mongoose.Schema({
   Order_id: {
-    type: Number,
-    required: true
+    type: mongoose.Schema.Types.ObjectId,
+    default: mongoose.Types.ObjectId,
+    unique: true,
   },
   User_id: {
     type: mongoose.Schema.Types.ObjectId,
-     required: true,
+    required: true,
      ref: 'User'
   },
   Dish_id: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
-    ref: 'Dish'
+    ref: 'Dishes'
   },
   Quantity: {
     type: Number,
     required: true
   },
-  Order_status: {
+  onanimationendrderStatus: {
     type: String,
     required: true
   },
-  Order_date: {
-    type: String,
+  orderDate: {
+    type: Date,
+    default: Date.now,
     required: true
   },
-  Other_info: {
+  otherInfo: {
     type: String,
   }
-});
+},
+{
+  timestamps: true,
+}
+);
 
 module.exports = mongoose.model('orders', ordersSchema);
 
