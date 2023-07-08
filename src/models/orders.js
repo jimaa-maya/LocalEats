f18e7acce54e17751b3c35a716b97e5c949b6277
Mohamad Aid Bacuk Zanab <mohammedsharif067@gmail.com>
2023-07-08 17:44:30 +0300

    Update Orders Schema

---
+++ src/models/orders.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const ordersSchema = new mongoose.Schema({
+  order_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: mongoose.Types.ObjectId,
+    unique: true,
+  },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+     ref: 'User'
+  },
+  dish_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Dishes'
+  },
+  quantity: {
+    type: Number,
+    required: true
+  },
+  orderStatus: {
+    type: String,
+    required: true
+  },
+  otherInfo: {
+    type: String,
+  }
+},
+{
+  timestamps: true,
+}
+);
+
+module.exports = mongoose.model('orders', ordersSchema);
