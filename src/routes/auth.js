9977e78d676173d909a6d87e3877f02b567c2b6c
nezirAydin <nezir.aydin94@gmail.com>
2023-07-11 15:13:25 +0300

    add facebook sign in

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -13,8 +13,40 @@ routes.get(
 );
 
 routes.get(
+  '/facebook',
+  passport.authenticate('facebook', {
+    scope: ['profile', 'email', 'openid'],
+  })
+);
+
+routes.get(
   '/google/callback',
   passport.authenticate('google', { session: false }),
+  (req, res) => {
+    const user = req.user;
+    console.log(user);
+    const token = jwt.sign(
+      {
+        userName: user.userName,
+        email: user.email,
+        providerId: user._id,
+      },
+      process.env.SECRET_KEY,
+      { expiresIn: '14d' }
+    );
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+    });
+
+    res.redirect('/');
+  }
+);
+
+routes.get(
+  '/facebook/callback',
+  passport.authenticate('facebook', { session: false }),
   (req, res) => {
     const user = req.user;
     console.log(user);
