1ee6ea0e4f5ab86e9c15263d95bfe42425b82296
Cansu Aysagdic <cansu.aysagdic@ogr.iu.edu.tr>
2023-07-11 21:22:17 +0300

    #36 added dishesTypand test folder

--- src/models/orders.js
+++ src/models/orders.js
@@ -1,43 +1,42 @@
 const mongoose = require('mongoose');
 
-const ordersSchema = new mongoose.Schema({
-  order_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    default: mongoose.Types.ObjectId,
-    unique: true,
+const ordersSchema = new mongoose.Schema(
+  {
+    order_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: mongoose.Types.ObjectId,
+      unique: true,
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    dish_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Dishes',
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    orderStatus: {
+      type: String,
+      required: true,
+    },
+    orderDate: {
+      type: Date,
+      default: Date.now,
+      required: true,
+    },
+    otherInfo: {
+      type: String,
+    },
   },
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-     ref: 'User'
-  },
-  dish_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'Dishes'
-  },
-  quantity: {
-    type: Number,
-    required: true
-  },
-  orderStatus: {
-    type: String,
-    required: true
-  },
-  orderDate: {
-    type: Date,
-    default: Date.now,
-    required: true
-  },
-  otherInfo: {
-    type: String,
+  {
+    timestamps: true,
   }
-},
-{
-  timestamps: true,
-}
 );
 
 module.exports = mongoose.model('orders', ordersSchema);
-
- 
