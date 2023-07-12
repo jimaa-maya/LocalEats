6fefd901356705f0dc6f150d24e4b1a4d6ca71d6
Cansu Aysagdic <cansu.aysagdic@ogr.iu.edu.tr>
2023-07-12 11:43:45 +0300

    #35 fixed defining token and removed role

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -1,19 +1,18 @@
 const jwt = require('jsonwebtoken');
 
-const generateToken = (response, user) => {
+const generateToken = (res, user) => {
   // eslint-disable-next-line camelcase
-  const { user_id, userName, email, role } = user;
+  const { user_id, userName, email } = user;
   const payload = {
     // eslint-disable-next-line camelcase
     user_id,
     userName,
     email,
-    role,
   };
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: '14d',
   });
-  response.cookie('token', token, {
+  res.cookie('token', token, {
     httpOnly: true,
     maxAge: 1000 * 3600 * 24 * 14,
     signed: true,
@@ -21,13 +20,13 @@ const generateToken = (response, user) => {
 };
 
 // eslint-disable-next-line consistent-return
-const authenticate = (request, response, next) => {
+const authenticate = (req, res, next) => {
   // Get the token from the request, typically from the "Authorization" header
-  const token = request.headers.authorization;
+  const token = req.cookies.jwt;
 
   // Check if a token is present
   if (!token) {
-    return response.status(401).json({ error: 'Unauthorized' });
+    return res.status(401).json({ error: 'Unauthorized' });
   }
 
   try {
@@ -35,13 +34,13 @@ const authenticate = (request, response, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     // Attach the decoded token to the request object
-    request.user = decoded;
+    req.user = decoded;
 
     // Call the next middleware or route handler
     next();
   } catch (error) {
     // Handle token verification error
-    return response.status(401).json({ error: 'Invalid token' });
+    return res.status(401).json({ error: 'Invalid token' });
   }
 };
 
