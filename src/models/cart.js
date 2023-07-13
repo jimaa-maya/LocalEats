08c0ebea553a9553283d032aa88d6dba0f6af4af
Cansu Aysagdic <118272563+cansucreates@users.noreply.github.com>
2023-07-13 10:31:28 +0300

    small fix

--- src/models/cart.js
+++ src/models/cart.js
@@ -1,30 +1,35 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema(
+    {
+        cart_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            default: mongoose.Types.ObjectId,
+            unique: true,
+        },
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: false,
+            ref: 'User',
+        },
+        dish_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Dishes',
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: ['active', 'inactive', 'completed'],
+            default: 'active',
+        },
     },
-    user_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'User',
-    },
-    dish_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'Dishes',
-    },
-    quantity: {
-      type: Number,
-      required: true,
-    },
-    status: {
-      type: String,
-      enum: ['active', 'inactive', 'completed'],
-      default: 'active',
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 module.exports = mongoose.model('Cart', cartSchema);
