1b9dab1461ae16b7a72594d4b71561b067efb656
Abdurrahman <abdurrahman.abzd@gmail.com>
2023-07-12 23:51:39 -0400

    Add password hashing test

---
+++ test/usersTest.js
@@ -0,0 +1,36 @@
+var mongoose = require(mongoose),
+    User = require(./src/models/users);
+
+var connStr = mongodb:
+mongoose.connect(connStr, function(err) {
+    if (err) throw err;
+    console.log(Successfully connected to MongoDB);
+});
+
+// create a user a new user
+var testUser = new User({
+    userName: Abdurrahman,
+    password: Password99;
+});
+
+// save user to database
+testUser.save(function(err) {
+    if (err) throw err;
+})
+
+// fetch user and test password verification
+User.findOne({ userName: 'Abdurrahman' }, function(err, user) {
+    if (err) throw err;
+
+    // test a matching password
+    user.comparePassword('Password99', function(err, isMatch) {
+        if (err) throw err;
+        console.log('Password123:', isMatch); // -> Password99: true
+    });
+
+    // test a failing password
+    user.comparePassword('123Password', function(err, isMatch) {
+        if (err) throw err;
+        console.log('123Password:', isMatch); // -> 123Password: false
+    });
+});
