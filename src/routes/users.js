8f10fce998df4936d778da13603a19c5e647b0d6
nezirAydin <nezir.aydin94@gmail.com>
2023-07-13 14:32:33 +0300

    add get role & address routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const routes = express.Router();
 const User = require('../controllers/users');
-const checkAuth = require('../middleware/checkAuth')
+const checkAuth = require('../middleware/checkAuth');
 routes.get('/',checkAuth.authenticate,User.getUser);
 routes.put('/', checkAuth.authenticate,User.putUser);
-routes.put('/addrole', checkAuth.authenticate,User.addRole);
-// get the role of the user
-// set the address for the user
-
+routes.put('/role', checkAuth.authenticate,User.addRole);
+routes.get('/role',checkAuth.authenticate, User.addRole)
 routes.put('/address', checkAuth.authenticate, User.addAddress);
+routes.get('/address',checkAuth.authenticate,User.getAddress);
 
-// get the address for the user
-/**************/
 
 module.exports = routes;
