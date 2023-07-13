c0ae5d63c4a7b49bae013faa420c7c210aee276c
Cansu Aysagdic <118272563+cansucreates@users.noreply.github.com>
2023-07-13 10:33:28 +0300

    small fixes

--- src/models/orders.js
+++ src/models/orders.js
@@ -1,41 +1,37 @@
 const mongoose = require('mongoose');
 
-const ordersSchema = new mongoose.Schema(
-  {
-    order_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      default: mongoose.Types.ObjectId,
-      unique: true,
-    },
-    user_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'User',
-    },
-    dish_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'Dishes',
-    },
-    quantity: {
-      type: Number,
-      required: true,
-    },
-    orderStatus: {
-      type: String,
-      required: true,
-    },
-    orderDate: {
-      type: Date,
-      default: Date.now,
-      required: true,
-    },
-    otherInfo: {
-      type: String,
-    },
+const ordersSchema = new mongoose.Schema({
+  order_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: mongoose.Types.ObjectId,
+    unique: true,
   },
-
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+     ref: 'User'
+  },
+  dish_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Dishes'
+  },
+  quantity: {
+    type: Number,
+    required: true
+  },
+  orderStatus: {
+    type: String,
+    required: true
+  },
+  otherInfo: {
+    type: String,
   }
+},
+{
+  timestamps: true,
+}
 );
 
+module.exports = mongoose.model('Orders', ordersSchema);
 
