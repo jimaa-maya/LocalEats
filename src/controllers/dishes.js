82b8c4db39221c0ececd7985a2498bd602a7e9a3
Cansu Aysagdic <cansu.aysagdic@ogr.iu.edu.tr>
2023-07-17 15:03:03 +0300

    added remaining controllers

--- src/controllers/dishes.js
+++ src/controllers/dishes.js
@@ -8,14 +8,14 @@ const Dishes = require('../models/dishes');
 // put controllers: (user:cook update dish image, user: cook update dish, update review based on its id
 // user: user but put spesific timeframe or limit the number of times a review can be modified.)
 
-// get controllers
+// public get controllers
 // getting all dishes
 
 const getAllDishes = async (req, res) => {
   try {
     const dishes = await Dishes.find();
     res.json(dishes);
-  } catch (err) {
+  } catch (error) {
     res.status(422).json({ message: 'The dish name is required' });
   }
 };
@@ -61,14 +61,14 @@ const getDishById = async (req, res) => {
     const dish = await Dishes.findById(id);
 
     if (!dish) {
-      res
+      return res
         .status(404)
         .json({ message: 'The dish you are looking for was not found' });
-    } else {
-      res.json(dish);
     }
-  } catch (err) {
-    res
+
+    return res.json(dish);
+  } catch (error) {
+    return res
       .status(500)
       .json({ message: 'An error occured while fetching the dish' });
   }
@@ -100,9 +100,65 @@ const fetchAllDishImages = async (req, res) => {
   }
 };
 
+// getting a single image by its id
+
+const fetchDishImage = async (req, res) => {
+  const { dishId } = req.params;
+
+  try {
+    const dish = await Dishes.findById(dishId, 'image_url');
+
+    if (!dish) {
+      return res.status(404).json({ message: 'Dish not found' });
+    }
+
+    if (!dish.image_url) {
+      return res.status(404).json({ message: 'Dish image not available' });
+    }
+
+    // if dish and image exists convert buffer data to base64
+
+    const imageBase64 = dish.image_url.toString('base64');
+    const imageUrl = `data:image/jpeg;base64,${imageBase64}`;
+
+    return res.status(200).json({ dishId: dish.dish_id, imageUrl });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+// Searching dishes
+
+const searchDishes = async (req, res) => {
+  const { query } = req.params;
+
+  try {
+    const dishes = await Dishes.find({
+      $or: [
+        { dishName: { $regex: query, $options: 'i' } }, // Case-insensitivity search for the name
+        { description: { $regex: query, $options: 'i' } }, // Case-insensitivity search for the descpription
+      ],
+    });
+
+    if (dishes.length === 0) {
+      return res
+        .status(404)
+        .json({ message: 'No dishes found matching the search query' });
+    }
+
+    return res.json(dishes);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: 'Internal server error. Please try again.' });
+  }
+};
+
 module.exports = {
   getAllDishes,
   filterDishes,
   getDishById,
   fetchAllDishImages,
+  fetchDishImage,
+  searchDishes,
 };
