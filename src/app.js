518c90b8cb0fed7f2b724de3f72add0199f40531
Abdurrahman <abdurrahman.abzd@gmail.com>
2023-07-21 00:14:37 -0400

    Connect routes to Express application

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-
 require('dotenv').config();
 const express = require('express');
 const passport = require('passport')
@@ -11,10 +10,15 @@ const apiRoutes = require('./routes');
 const app = express();
 const port = 3000;
 
-app.use(express.json())
+// Parse JSON bodies
+app.use(express.json());
+
 app.set("view engine","ejs");
 app.set("views", "./views");
 app.use(passport.initialize());
+
+// Mount all routes from the routes directory under the '/api' path
+app.use('/api', apiRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const routes = express.Router();
 
+const userRoutes = require('./users');
+const dishesRoutes = require('./dishes');
+const ordersRoutes = require('./orders');
+const cartRoutes = require('./cart');
+const authRoutes = require('./auth');
 const authRoutes = require('./auth');
 const userRoutes = require('./users');
 
 routes.use('/auth', authRoutes);
 routes.use('/user', userRoutes);
+router.use('/dishes', dishesRoutes);
+router.use('/orders', ordersRoutes);
+router.use('/cart', cartRoutes);
 
 
 module.exports = routes;
