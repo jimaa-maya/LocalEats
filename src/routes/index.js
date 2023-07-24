78e971f2added8f42e4126c3642dbeef3eda5f80
Mohamad Aid Bacuk Zanab <mohammedsharif067@gmail.com>
2023-07-24 09:46:14 +0300

    adding routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@ const routes = express.Router();
 const authRoutes = require('./auth');
 const userRoutes = require('./users');
 const signRoutes = require('./sign')
+const ordersRoutes = require('./orders');
 
 routes.use('/auth', authRoutes);
 routes.use('/sign', signRoutes);
 routes.use('/user', userRoutes);
+routes.use('/orders', ordersRoutes);
 
 
 module.exports = routes;
