1d0f6ecd5a73604c3d325be0eadb61c43a8b6c97
Mohamad Aid <128480137+mohamadAid@users.noreply.github.com>
2023-07-26 14:28:38 +0300

    #49 Orders controllers (#62)
    * Adding CRUD to Orders.
    
    * adding routes
    
    ---------
    
    Co-authored-by: Cansu Aysagdic <118272563+cansucreates@users.noreply.github.com>

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,16 +5,21 @@ const routes = express.Router();
 const authRoutes = require('./auth');
 const userRoutes = require('./users');
 
+const signRoutes = require('./sign')
+const ordersRoutes = require('./orders');
 const dishRoutes = require('./dishes');
 const signRoutes = require('./sign');
 const signRoutes = require('./sign');
 const cartRoutes = require('./cart')
 
 
+
 routes.use('/auth', authRoutes);
 routes.use('/sign', signRoutes);
 routes.use('/user', userRoutes);
+routes.use('/orders', ordersRoutes);
 routes.use('/dishes', dishRoutes);
 routes.use('/cart', cartRoutes);
+
 
 module.exports = routes;
