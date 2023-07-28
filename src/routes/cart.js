0c9cd24469d7e674cbc104d09a346d62f55fac66
nezirAydin <nezir.aydin94@gmail.com>
2023-07-28 16:50:45 +0300

    add post req & schema swagger

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -20,6 +20,32 @@ const cartService = require('../utils/cartService');
 *              finished: true
 */
 
+/**
+ * @swagger
+ * tags:
+ *   name: Books
+ *   description: The books managing API
+ * /books:
+ *   post:
+ *     summary: Create a new book
+ *     tags: [Books]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       200:
+ *         description: The created book.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       500:
+ *         description: Some server error
+ *
+ */
 
 routes.post('/cart', cartController.createCart);
 routes.get('/cartItems', cartController.getCartItems);
